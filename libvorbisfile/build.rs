1284622c185d9c3f96b51ae99da6d5b242faff29
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-02-12 21:29:21 +0100

    Update

--- libvorbisfile/build.rs
+++ libvorbisfile/build.rs
@@ -3,16 +3,18 @@ extern crate gcc;
 
 fn main() {
     match pkg_config::find_library("vorbisfile") {
-        Ok(()) => return,
+        Ok(_) => return,
         Err(..) => {}
     };
+
+    println!("{:?}", std::env::var("DEP_VORBIS_INCLUDE"));
 
     gcc::Config::new()
                 .file("libvorbisfile/vorbisfile.c")
                 .define("_USRDLL", None)
                 .define("LIBVORBIS_EXPORTS", None)
-                .include(Path::new(std::env::var_string("DEP_VORBIS_INCLUDE").unwrap()))
-                .include(Path::new(std::env::var_string("DEP_VORBIS_SRC").unwrap()))
-                .include(Path::new(std::env::var_string("DEP_OGG_INCLUDE").unwrap()))
+                .include(Path::new(std::env::var("DEP_VORBIS_INCLUDE").unwrap()))
+                .include(Path::new(std::env::var("DEP_VORBIS_SRC").unwrap()))
+                .include(Path::new(std::env::var("DEP_OGG_INCLUDE").unwrap()))
                 .compile("libvorbisfile.a");
 }
